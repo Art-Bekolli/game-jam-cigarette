bd92a69c20fef56ea662619a50934f3b5cdd60bb
no4r <101732480+no4rr@users.noreply.github.com>
2026-08-09 08:27:17 +0200

    E kom shtu ket njerin si sprite po mu ka prish text boxi spo punon mo

---
+++ randomi.gd
@@ -0,0 +1,46 @@
+extends Node3D
+
+@onready var dialog_controller: CanvasLayer = $"../../DialogController2"
+
+
+func tipiShanca() -> int:
+	var shanca = {
+		"Miri": 800,
+		"MiriShume": 200,
+		"Keqi": 500,
+		"KeqiShume": 150
+	}
+
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+
+	# 1. Calculate weighted sum (1650 total)
+	var weightedSum: int = 0
+	for key in shanca:
+		weightedSum += shanca[key]
+
+	# 2. Pick random roll from 1 to 1650
+	var tipiType: int = rng.randi_range(1, weightedSum)
+	var selected_rarity: String = "Miri"
+
+	# 3. Correct Weighted Roll Logic
+	for key in shanca:
+		var weight = shanca[key]
+		if tipiType <= weight:
+			selected_rarity = key
+			break
+		tipiType -= weight
+		
+
+	# 4. Map to integer ID
+	match selected_rarity:
+		"Miri":
+			return 0
+		"MiriShume":
+			return 1
+		"Keqi":
+			return 2
+		"KeqiShume":
+			return 3
+		_:
+			return 0
